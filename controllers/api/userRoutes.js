52b2bbd4c4fd35595ece4106a489809d388d4948
Eli Wood <mreliwood1@gmail.com>
2023-07-25 14:31:09 -0700

    merged

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import apiAuth from '../../middleware/apiAuth.js';
+
 const router = express.Router();
 
 // POST user login
@@ -13,6 +15,15 @@ router.post('/login', (req, res) => {
 
 		const token = jwt.sign({ loggedIn: true }, process.env.JWT_SECRET, { expiresIn: '12h' });
 		return res.status(200).json({ token });
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({ error });
+	}
+});
+
+router.post('/checktoken', apiAuth, (req, res) => {
+	try {
+		return res.status(200).json(req.loggedIn);
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({ error });
