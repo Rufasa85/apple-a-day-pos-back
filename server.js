2d0cc6e1545872bde359955e10183abd8a70c47a
Joe Rehfuss <joe.rehfuss@gmail.com>
2023-08-30 16:16:29 -0700

    fixed

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.use((req, res, next) => {
-//   const allowedOrigins = ['http://localhost:3000', ]; // add deployed URL here
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader('Access-Control-Allow-Origin', origin);
-//   }
-//   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   return next();
-// });
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000',"https://apple-a-day-cafe.netlify.app" ]; // add deployed URL here
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 
-// app.options('*', (req,res) => { res.sendStatus(200) });
+app.options('*', (req,res) => { res.sendStatus(200) });
 
 app.use(routes);
 
